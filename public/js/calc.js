4bb100446d98346c69357dc9b4b9020ed2a366fc
Matthew Kotys <mattkotys@gmail.com>
2017-12-04 17:24:16 -0600

    Needs testing

--- public/js/calc.js
+++ public/js/calc.js
@@ -1,47 +1,77 @@
 'use strict'
+function calculateMWW() {
 
-sf = () => {
-    if (!isNaN(triceps) && !isNaN(subscapula) && !isNaN(abdominal)) {
-        return(triceps + subscapula + abdominal)
+    let sex = document.querySelector('input[name = "sex"]:checked').value
+    let weight = document.getElementById("weight").value
+    let triceps = document.getElementById("triceps").value
+    let subscapula = document.getElementById("subscapula").value
+    let abdominal = document.getElementById("abdominal").value
+    let abdominalOption = document.getElementById("abdominalOption")
+
+    let sf
+    if (sex ==="m") {
+        sf = (triceps > 0 && subscapula > 0 && abdominal > 0) ? +triceps + +subscapula + +abdominal : 0
+        abdominalOption.style.display = "block"
     } else {
-        return(0)
+        sf = (triceps > 0 && subscapula > 0) ? +triceps + +subscapula : 0
+        abdominalOption.style.display = "none"
     }
-}
-bd = () => {
-    if (sf() > 0) {
-        return((1.0973 - sf() * 0.000815) + (Math.pow(sf(), 2) * 0.00000084))
+    let bd = (sf > 0) ? (1.0973 - sf * 0.000815) + (Math.pow(sf, 2) * 0.00000084) : 0
+
+    let bf
+    if (sex ==="m") {
+        bf = (bd > 0) ? ((457/bd) - (4.142) * 100) : 0
     } else {
-        return(0)
+        bf = (bd > 0) ? 1.35 * sf - 0.012 * Math.pow(sf, 2) - 3.4 : 0
     }
-}
-bf = () => {
-    if (sf() > 0) {
-        return((((457/bd()) - (4.142) * 100)))
-    } else {
-        return (0)
-    }
-}
-fw = () => {
-    if (sf() > 0 && weight > 0) {
-        return( weight * (bf() / 100))
-    } else {
-        return (0)
-    }
-}
-lbm = () => {
-    if (sf() > 0 && weight > 0) {
-        return( weight - fw())
-    } else {
-        return (0)
-    }
-}
-mww = () => {
-    let quotient = 1;
-    if (sex === "m") { quotient = 0.93 } else { quotient = 0.88 }
-    if (sf() > 0 && weight > 0) {
-        return( lbm() / quotient )
-    } else {
-        return (0)
+
+    let fw = (sf > 0 && weight > 0) ? weight * bf / 100 : 0
+    let lbm = (sf > 0 && weight > 0) ? weight - fw : 0
+    let quotient = (sex === "m") ? 0.93 : 0.88
+    let mww = (sf > 0 && weight > 0) ? lbm / quotient : 0
+
+    document.getElementById("mww").value = mww.toFixed(2)
+
+    let calculations = document.getElementById("calculations")
+    calculations.innerHTML = "<ul>" +
+                        "<li>SF:" + sf +
+                        "</li>" +
+                        "<li>Body Density: " + bd.toFixed(2) +
+                        "</li>" +
+                        "<li>Body Fat: " + bf.toFixed(2) + "%" +
+                        "</li>" +
+                        "<li>Lean Body Mass: " + lbm.toFixed(2) +
+                        "</li>" +
+                        "</ul>"
+
+    let weightLossSchedule = document.getElementById("weightLossSchedule").getElementsByTagName("tbody")[0]
+    weightLossSchedule.innerHTML = ""
+    let w, x, y, z
+    let currentWeight = +weight
+    let previousWeight = currentWeight
+    let percentLost = 0
+    let i = 0
+    let newRow
+    //console.log("Minimum Weight: " + mww + ", Current Weight: " + currentWeight)
+
+    if (weight > mww && weight - mww < 100) {
+        while (currentWeight > mww) {
+            //console.log(weightLossSchedule)
+            currentWeight -= weight * 0.015
+            i++
+            newRow = weightLossSchedule.insertRow(weightLossSchedule.rows.length)
+            w = newRow.insertCell(0)
+            w.innerHTML = i
+            x = newRow.insertCell(1)
+            x.innerHTML = (currentWeight).toFixed(2)
+            y = newRow.insertCell(2)
+            y.innerHTML = (weight - currentWeight).toFixed(2)
+            z = newRow.insertCell(3)
+            z.innerHTML = percentLost.toFixed(2)
+
+            percentLost = (previousWeight - currentWeight) / previousWeight * 100
+            previousWeight = currentWeight
+        }
     }
 }
 
@@ -61,37 +91,9 @@ mww = () => {
 // %BF=1.35 x (sum SF)-0.012 x (sum SF)2 -3.4
 // Sum of SF = Triceps SF + Subscapula SF
 // MWW (Females) = (LBM) / .88
-
-function calculatePayment() {
-let principal = document.getElementById("principal").value
-let rate = document.getElementById("rate").value / 100
-let term = document.getElementById("term").value
-let payment = principal * rate * Math.pow((1.0 + rate), term) / (Math.pow((1.0 + rate), term) - 1.0)
-let priorPrincipal = principal
-let currentPrincipal = principal
-let tableRef
-let newRow
-let x
-let y
-let z
-
-document.getElementById("payment").value = payment.toFixed(2)
-
-tableRef = document.getElementById("amortizationSchedule").getElementsByTagName("tbody")[0]
-tableRef.innerHTML = ""
-
-for (let i=1; i<term; i++) {
-    priorPrincipal = currentPrincipal * (1 + rate) - payment
-    currentPrincipal = priorPrincipal
-    // i  priorPrincipal.toFixed(2) (payment * i).toFixed(2)
-    newRow = tableRef.insertRow(tableRef.rows.length)
-    x = newRow.insertCell(0)
-    x.innerHTML = i
-    y = newRow.insertCell(1)
-    y.innerHTML = priorPrincipal.toFixed(2)
-    z = newRow.insertCell(2)
-    z.innerHTML = (payment * i).toFixed(2)
-}
-
-
-}
+//
+// A weight loss of 1.5% of a wrestler’s total body weight per week on the descent, has been established
+// by the IHSA. A wrestler will not be allowed to wrestle at the established minimum weight until the
+// date specified on the body fat result sheet provided by the IHSA. These dates allow for a descent of
+// 1.5% weight loss per week from the date of the body fat testing.
+//
